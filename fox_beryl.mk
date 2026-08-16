a8b59cad54cf1ccb68c2434a160b7416c537f127
itswill00 <anstykx00@gmail.com>
2026-08-16 07:18:35 +0000

    fox_beryl: Remove quotes from flashlight sysfs paths
    - Remove double quotes from OF_FL_PATH1 and OF_FL_PATH2 to prevent syntax errors during C++ macro expansion in recovery compilation.
    
    Signed-off-by: itswill00 <anstykx00@gmail.com>
    
    Change-Id: I3c6bd527793c6e73c922c66fa17d814326fd6113

--- fox_beryl.mk
+++ fox_beryl.mk
@@ -61,8 +61,8 @@ OF_DYNAMIC_FULL_SIZE := 9126805504
 
 # Flashlight
 OF_FLASHLIGHT_ENABLE := 1
-OF_FL_PATH1 := "/sys/class/flashlight/flashlight/flashlight_brightness"
-OF_FL_PATH2 := "/sys/class/leds/flashlight/brightness"
+OF_FL_PATH1 := /sys/class/flashlight/flashlight/flashlight_brightness
+OF_FL_PATH2 := /sys/class/leds/flashlight/brightness
 
 # number of list options before scrollbar creation
 OF_OPTIONS_LIST_NUM := 11
